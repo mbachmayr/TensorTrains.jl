3bd4dd49fee934f275d7964328258e02f2354b21
Markus Bachmayr <bachmayr@uni-mainz.de>
2019-12-03 17:57:40 +0100

    TTPy fix

--- src/TTPy.jl
+++ src/TTPy.jl
@@ -10,30 +10,28 @@
 using PyCall, Logging
 using TensorTrains
 
+import TensorTrains.Tensor, TensorTrains.TensorMatrix
+
 export pyvec, pymat, Tensor, TensorMatrix, amen
 
-pyvec(t::Tensor) = nothing
-pymat(t::TensorMatrix) = nothing
+tt = PyNULL()
+amen_pkg = PyNULL()
 
-Tensor(x::PyObject) = nothing
-TensorMatrix(x::PyObject) = nothing
+pyvec(t::Tensor) = tt.vector.from_list(t)
+pymat(t::TensorMatrix) = tt.matrix.from_list(t)
 
-amen(A::TensorMatrix, b::Tensor, x::Tensor, ɛ::Float64,
-    kickrank::Integer = 4, nswp::Integer = 20) = nothing
-
-try
-tt = pyimport("tt")
-amen_pkg = pyimport("tt.amen")
-
-pyvec(t::Tensor) = tt.vector[:from_list](t)
-pymat(t::TensorMatrix) = tt.matrix[:from_list](t)
-
-Tensor(x::PyObject) = Tensor(tt.vector[:to_list](x))
-TensorMatrix(x::PyObject) = TensorMatrix(tt.matrix[:to_list](x))
+Tensor(x::PyObject) = Tensor(tt.vector.to_list(x))
+TensorMatrix(x::PyObject) = TensorMatrix(tt.matrix.to_list(x))
 
 amen(A::TensorMatrix, b::Tensor, x::Tensor, ɛ::Float64,
-    kickrank::Integer = 4, nswp::Integer = 20) =
-  Tensor(amen_pkg.amen_solve(pymat(A), pyvec(b), pyvec(x), ɛ, kickrank, nswp))
-catch;
-    @warn "import of Python TT Toolbox failed"
+		    kickrank::Integer = 4, nswp::Integer = 20) =
+		  Tensor(amen_pkg.amen_solve(pymat(A), pyvec(b), pyvec(x), ɛ, kickrank, nswp))
+
+function __init__()
+	try
+		copy!(tt, pyimport("tt"))
+		copy!(amen_pkg, pyimport("tt.amen"))
+	catch;
+	    @warn "import of Python TT Toolbox failed"
+	end
 end
